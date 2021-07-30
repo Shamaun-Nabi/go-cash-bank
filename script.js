d2647def792c870aefbbf13ff9aa75b4dfef07a4
Shamaun-Nabi <snashik950@gmail.com>
2021-07-31 00:31:55 +0600

    deposite and withdraw money

--- script.js
+++ script.js
@@ -1,9 +1,49 @@
+// Login Box area
 const loginbtn = document.getElementById("loginbtn");
 
-loginbtn.addEventListener('click',function(event){
-    const welcomePage=document.getElementById("welcomePage");
-    welcomePage.style.display="none";
-    const trasenctionArea=document.getElementById("Transiction-area");
-    trasenctionArea.style.display="block";
+loginbtn.addEventListener('click', function (event) {
+    const welcomePage = document.getElementById("welcomePage");
+    welcomePage.style.display = "none";
+    const trasenctionArea = document.getElementById("Transiction-area");
+    trasenctionArea.style.display = "block";
 
 })
+
+
+// Transaction AREA
+
+// deposit
+const addDeposit = document.getElementById("addDeposit");
+addDeposit.addEventListener("click", function () {
+    // Taking input from Html Input form
+    const depositAmount = document.getElementById("depositAmount").value;
+    const rcvValue = parseFloat(depositAmount);
+    console.log(rcvValue);
+    updateMoney("showDeposit", rcvValue);
+    updateMoney("show", rcvValue);
+    document.getElementById("depositAmount").value = "";
+
+
+})
+
+
+
+// withdraw
+const withdrawAmount = document.getElementById("withdraw");
+withdrawAmount.addEventListener("click", function () {
+    const withdrawNumber = document.getElementById("withdrawAmount").value;
+    const makeAmount = parseFloat(withdrawNumber);
+   updateMoney("showWithdraw", makeAmount);
+   updateMoney("show", -1*makeAmount);
+})
+
+
+// update function
+function updateMoney(id, depositAmount) {
+    // taking input from HTML Tag
+    const currentShowingAmount = document.getElementById(id).innerText;
+    const makeShowingAmount = parseFloat(currentShowingAmount);
+    const total = depositAmount + makeShowingAmount;
+    document.getElementById(id).innerText = total;
+
+}
